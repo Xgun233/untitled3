9057f5e2d9fd174990a58f8e35642783141b362c
zhangshaocong <2372807518@qq.com>
2022-11-05 19:57:13 +0800

    second commit

--- src/javatest/GitTest.java
+++ src/javatest/GitTest.java
@@ -6,6 +6,7 @@ package javatest;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello,World");
+        System.out.println("Hello,git1");
+        System.out.println("Hello,git2");
     }
 }
