3836554d9ab24a538032617fb7c6b421df7ff3d5
zhangshaocong <2372807518@qq.com>
2022-11-05 19:51:38 +0800

    first commit

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+/**
+ * 作者：聪哥
+ * 描述： 永无bug
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
---
+++ src/javatest/GitTest.java
@@ -0,0 +1,11 @@
+package javatest;
+
+/**
+ * 作者：聪哥
+ * 描述： 永无bug
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+    }
+}
